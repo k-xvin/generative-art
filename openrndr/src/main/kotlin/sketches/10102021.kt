648aa20c248c64d57d55b985c973a10076f177eb
pisslake <74090716+pisslake@users.noreply.github.com>
2021-10-10 21:12:25 -0700

    added sin waving

---
+++ openrndr/src/main/kotlin/sketches/10102021.kt
@@ -0,0 +1,69 @@
+package sketches;
+
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.Drawer
+import org.openrndr.draw.LineCap
+import org.openrndr.math.Vector2
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sin
+
+fun main() = application {
+    configure {
+        width = 800
+        height = 800
+    }
+
+    program {
+        // https://coolors.co/8c1c13-bf4342-e7d7c1-a78a7f-735751
+        val hex = arrayOf("8c1c13", "bf4342", "e7d7c1", "a78a7f", "735751");
+        val palette = hex.map { ColorRGBa.fromHex(it) }
+
+        extend {
+            drawer.translate(width / 2.0, height / 2.0)
+            drawer.clear(ColorRGBa.WHITE)
+
+            drawer.strokeWeight = 5.0;
+            drawer.stroke = palette[3];
+            drawer.lineCap = LineCap.ROUND;
+            drawer.sineWave(
+                Vector2(0.0, 0.0),
+                Vector2(400.0, -200.0),
+                500,
+                SineWave(0.04, 0.0, 20.0)
+            );
+
+
+        }
+    }
+}
+
+fun Drawer.sineWave(start: Vector2, end: Vector2, numPoints: Int, sineWave: SineWave) {
+    this.lineSegment(start, end);
+
+    val distance = start.distanceTo(end);
+    val step = distance / numPoints;
+
+    val angle = atan2(end.y,end.x);
+
+    val points = List(numPoints) { t ->
+        val movedStartX = step * t + start.x;
+        val movedStartY = sineWave.amp * sin(sineWave.freq*t.toDouble()) + start.y;
+
+        val rotatedX = 0.0;
+        val rotatedY = 0.0;
+        Vector2(rotatedX, rotatedY);
+    }
+
+
+
+    this.lineStrip(points);
+}
+
+data class SineWave(val freq: Double, val shift: Double, val amp: Double) {
+//    fun value(t: Double, x: Double) = amp * sin(t * freq + shift * x)
+}
+
+
+
