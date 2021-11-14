d942139dc66bf52860b2bf738dc6480cb3de77b8
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-11-14 10:41:28 -0800

    recording for centerwave

--- openrndr/src/main/kotlin/sketches/CenterWave.kt
+++ openrndr/src/main/kotlin/sketches/CenterWave.kt
@@ -3,17 +3,27 @@ package sketches
 import org.openrndr.*
 import org.openrndr.color.ColorRGBa
 import org.openrndr.extra.noise.simplex
-import org.openrndr.extra.olive.oliveProgram
+import org.openrndr.extra.videoprofiles.ProresProfile
+import org.openrndr.ffmpeg.ScreenRecorder
 import org.openrndr.math.Vector2
 
 fun main() = application {
     configure {
-        width = 800
-        height = 800
+//        width = 800
+//        height = 800
+        width = 1920
+        height = 1080
+        fullscreen = Fullscreen.SET_DISPLAY_MODE
     }
-    //program {
-    oliveProgram {
+    program {
+//        oliveProgram {
 
+        extend(ScreenRecorder()){
+            profile = ProresProfile().apply {
+                profile = ProresProfile.Profile.HQ4444
+                codec = "prores_ks"
+            }
+        }
 
         val spacecadet = ColorRGBa.fromHex("2E2D4D")
         val grullo = ColorRGBa.fromHex("A99F96")
@@ -24,7 +34,7 @@ fun main() = application {
         var radius = 10.0;
 //        val amp = radius
 //        var step = 0.003
-        var step = -0.001
+        var step = -0.004
 
         keyboard.keyDown.listen {
             // -- it refers to a KeyEvent instance here
@@ -73,17 +83,15 @@ fun main() = application {
             drawer.stroke = bloodred
             drawer.fill = grullo
 
-
-
             drawer.strokeWeight = 1.0;
             drawer.pushTransforms()
             drawer.translate(width / 2.0, height / 2.0)
             for (i in 0..360) {
                 drawer.pushTransforms()
-                val points = List(width /*/ 2*/) {
+                val points = List(width / 2) {
                     val x = it.toDouble()
                     val offset = simplex(i, (step * it) + seconds / 1.2) * radius
-                    Vector2(x, /*height / 2 +*/ offset)
+                    Vector2(x, offset)
                 }
                 drawer.rotate(i.toDouble())
                 drawer.lineStrip(points)
@@ -91,19 +99,12 @@ fun main() = application {
             }
             drawer.popTransforms()
 
-
-//            drawer.strokeWeight = 5.0;
-////            drawer.circle(Vector2(width / 2.0, height / 2.0), radius)
-//
-//            drawer.strokeWeight = 2.0;
-//            drawer.circle(Vector2(width / 2.0, height / 2.0), radius / 2)
-//
             drawer.fill = bloodred
             drawer.circle(Vector2(width / 2.0, height / 2.0), 10.0)
 
 
         }
     }
-
-
 }
+
+
